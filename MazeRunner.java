d5aa4fbe55fb1665ddafe9b6f54c1a63361a4f98
EllaDesai <152904052+EllaDesai@users.noreply.github.com>
2023-12-21 20:21:55 -0800

    Update MazeRunner.java

--- MazeRunner.java
+++ MazeRunner.java
@@ -1,1 +1,26 @@
+import javax.swing.JFrame;
+import java.awt.Component;
+import java.util.Scanner;
 
+public class MazeRunner extends JFrame
+{
+  private static int WIDTH = 600;
+  private static int HEIGHT = 600;
+
+  public MazeRunner(){
+    super("RatRace");
+    setSize(WIDTH,HEIGHT);
+    
+    Game theGame = new Game();
+    ((Component)theGame).setFocusable(true);
+
+    getContentPane().add(theGame);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+
+  public static void main(String args[]){
+    MazeRunner run = new MazeRunner();
+  }
+  
+}
